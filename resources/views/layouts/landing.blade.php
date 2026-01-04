a2aa3201c59abfc02967d3e8b02a0b84d30a2865
Dwi Susanto <otnasusdwi@gmail.com>
2026-01-04 15:57:54 +0700

    Update navbar logo image to jjb-orange-hitam

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -293,7 +293,7 @@
     <nav class="navbar navbar-expand-lg sticky-top">
         <div class="container">
             <a class="navbar-brand" href="{{ route('home') }}">
-                <img src="{{ asset('jjb.png') }}" alt="JJB Travel Services" style="height: 50px;">
+                <img src="{{ asset('jjb-orange-hitam.png') }}" alt="JJB Travel Services" style="height: 50px;">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
